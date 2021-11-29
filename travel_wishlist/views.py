b7408b042db0456714c5d0b4713292530fc8d262
mkhalil7625 <mkhalil7625@gmail.com>
2021-11-28 20:48:04 -0600

    part2 and tests

--- travel_wishlist/views.py
+++ travel_wishlist/views.py
@@ -16,6 +16,7 @@ def place_list(request):
     places = Place.objects.filter(visited=False).order_by('name')
     new_place_form = NewPlaceForm()
     return render(request, 'travel_wishlist/wishlist.html',{'places':places , 'new_place_form':new_place_form})
+
 def places_visited(request):
     visited = Place.objects.filter(visited=True)
     return render(request,'travel_wishlist/visited.html',{'visited':visited})
